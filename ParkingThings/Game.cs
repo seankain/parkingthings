086e2d94558a9bee9e6d50e9f772fc882f4dff34
unknown <sean.kain.geo@gmail.com>
2025-08-22 23:06:39 -0400

    didnt finish making game state singleton

---
+++ ParkingThings/Game.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+public partial class Game : Node
+{
+    public uint Level = 0;
+    public uint Score = 0;
+}
--- ParkingThings/Scenes/ParkingSpace.cs
+++ ParkingThings/Scenes/ParkingSpace.cs
@@ -1,16 +1,43 @@
 using Godot;
 using System;
+using System.ComponentModel;
 
 public partial class ParkingSpace : Node3D
 {
     [Export]
     public PackedScene VehicleScene;
 
+    [Export]
     private ParkingSpaceArea area;
+
+    private Game game;
+
+    [Export]
+    public Node3D FrontRightPost;
+    [Export]
+    public Node3D FrontLeftPost;
+    [Export]
+    public Node3D RearRightPost;
+    [Export]
+    public Node3D RearLeftPost;
 
     public override void _Ready()
     {
+        area.ParkingSpaceEntered += OnParkingSpaceEntered;
+        area.ParkingSpaceExited += OnParkingSpaceExited;
+        game = GetNode<Game>("/root/Game");
         base._Ready();
+    }
+
+    private void OnParkingSpaceExited(object sender, ParkingSpaceTransitEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    private void OnParkingSpaceEntered(object sender, ParkingSpaceTransitEventArgs e)
+    {
+        throw new NotImplementedException();
     }
 
 }
